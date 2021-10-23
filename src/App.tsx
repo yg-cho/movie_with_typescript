0fd355b2ba3603f0dc6be1ec67d3969816f6932d
ygcho <sorisori01@naver.com>
2021-10-23 15:24:14 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>abcde</h1>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
+
+// RSC
+import React from 'react';
+import "./App.css"
+
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>ABCDE</h1>
+        </div>
+    );
+};
+
+export default App;
+
+
